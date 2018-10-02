e00095faccfc47624997eab94017c2699a740082
bran <brantonli.ca@gmail.com>
2018-10-01 20:29:32 -0700

    q4 left
    finish all other tasks except q4

--- pa1_301311707.r
+++ pa1_301311707.r
@@ -8,73 +8,68 @@ library("clusteval")
 library("ggplot2")
 
 ##Task 1
-wine <- read.csv("wine.csv")
-wine.data = wine
+wine <- read.csv(file = "wine.csv")
+wine.data <- wine
 wine.data$Wine <- NULL
-#print(wine.data)
 
 ##Task 2
 #Z-score
-for(i in 1:nrow(wine.data)){
-  wine.data$Alcohol[i] <- (wine.data$Alcohol[i] - mean(wine.data$Alcohol))/sd(wine.data$Alcohol)
-  wine.data$Malic.acid[i] <- (wine.data$Malic.acid[i] - mean(wine.data$Malic.acid))/sd(wine.data$Malic.acid)
-  wine.data$Ash[i] <- (wine.data$Ash[i] - mean(wine.data$Ash))/sd(wine.data$Ash)
-  wine.data$Acl[i] <- (wine.data$Acl[i] - mean(wine.data$Acl))/sd(wine.data$Acl)
-  wine.data$Mg[i] <- (wine.data$Mg[i] - mean(wine.data$Mg))/sd(wine.data$Mg)
-  wine.data$Phenols[i] <- (wine.data$Phenols[i] - mean(wine.data$Phenols))/sd(wine.data$Phenols)
-  wine.data$Flavanoids[i] <- (wine.data$Flavanoids[i] - mean(wine.data$Flavanoids))/sd(wine.data$Flavanoids)
-  wine.data$Nonflavanoid.phenols[i] <- (wine.data$Nonflavanoid.phenols[i] - mean(wine.data$Nonflavanoid.phenols))/sd(wine.data$Nonflavanoid.phenols)
-  wine.data$Proanth[i] <- (wine.data$Proanth[i] - mean(wine.data$Proanth))/sd(wine.data$Proanth)
-  wine.data$Color.int[i] <- (wine.data$Color.int[i] - mean(wine.data$Color.int))/sd(wine.data$Color.int)
-  wine.data$Hue[i] <- (wine.data$Hue[i] - mean(wine.data$Hue))/sd(wine.data$Hue)
-  wine.data$OD[i] <- (wine.data$OD[i] - mean(wine.data$OD))/sd(wine.data$OD)
-  wine.data$Proline[i] <- (wine.data$Proline[i] - mean(wine.data$Proline))/sd(wine.data$Proline)
-}
-#m <- apply(wine.data,2,mean)
-#s <- apply(wine.data,2,sd)
-#wine.data <- scale(wine.data,m,s)
+m <- apply(wine.data,2,mean)
+s <- apply(wine.data,2,sd)
+wine.data <- scale(wine.data,m,s)
 
 ##Task 3
-k.wine <- kmeans(wine.data, centers = 4)
 distance <- dist(wine.data)
-plot(silhouette(k.wine$cluster, distance))
-#average silhouette width: 0.22
+sil_score <- function(center_num){
+  k.wine <- kmeans(wine.data, centers = center_num)
+  score <- silhouette(k.wine$cluster, distance)
+  mean_score <- mean(score[,3])
+  return(mean_score)
+}
+ss_4 <- sil_score(4)
+print(ss_4)
 
 ##Task 4
-plot(wine.data$Hue, wine.data$Phenols, main="Scatter Plot of Alcohol vs Proline"
-     ,xlab="Alcohol", ylab="Proline")
+cluster <- k.wine$cluster
+
+#Alcohol vs Flavanoids
+
+scat <- ggplot(data = wine, aes(x=Acl, y=Proline, color=cluster)) + geom_point()
+plot(scat)
 
 ##Task 5 
-k.wine <- kmeans(wine.data, centers =2)
-distance <- dist(wine.data)
-plot(silhouette(k.wine$cluster, distance))
-#silhouette score with diff centers [-1,1]
-#2: 0.52
-#3: 0.45
-#4: 0.22
-#5: 0.22
-#6: 0.24
-#7: 0.2
-#8: 0.19
-#9: 0.19
+ss_list = c(2:10)
+cluster_range = c(2:10)
+for(i in 2:10){
+  ss_list[i-1] <- sil_score(i)
+}
+plot(cluster_range, ss_list, main="Silhouette score vs # of Clusters", xlab="# of clusters"
+     , ylab="silhouette score")
 
 ##Task 6
 #complete linkage
 hc <- hclust(distance)
-plot(hc, label=wine$Wine, hang = -1)
+plot(hc, label=wine$Wine, hang = -1, main="Complete-link")
 
 #average
 ha <- hclust(distance, method="average")
-plot(ha, label=wine$Wine, hang = -1)
+plot(ha, label=wine$Wine, hang = -1, main="Average-link")
 
 #single
 hs <- hclust(distance, method="single")
-plot(hs, label=wine$Wine, hang = -1)
+plot(hs, label=wine$Wine, hang = -1, main="Single-link")
 
 ##Task 7
-new.hc <- cutree(hc,2)
-new.ha <- cutree(ha,2)
-new.hs <- cutree(hs,2)
+new.hc <- cutree(hc,3)
+new.ha <- cutree(ha,3)
+new.hs <- cutree(hs,3)
 
 ##Task 8
+sim_c <- cluster_similarity(wine$Wine, new.hc, similarity = "rand")
+print(sim_c)
 
+sim_a <- cluster_similarity(wine$Wine, new.ha, similarity = "rand")
+print(sim_a)
+
+sim_s <- cluster_similarity(wine$Wine, new.hs, similarity = "rand")
+print(sim_s)
